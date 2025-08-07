34171ef63f8b6521435aa428b4fe6c0fc04e0283
datagrape <info@datagrape.ai>
2025-08-08 03:31:32 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -1,10 +1,21 @@
+# import os
+
+
+# class Config:
+#     SECRET_KEY = "your-secret-key"
+#     basedir = os.path.abspath(os.path.dirname(__file__))
+#     db_path = os.path.join(basedir, 'instance', 'render.db')
+
+#     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 import os
 
-
 class Config:
-    SECRET_KEY = "your-secret-key"
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(basedir, 'instance', 'render.db')
+    SECRET_KEY = "jjkhjhkjvkvjhvjfjcjvhkhgcfjgccg"
+
+    # Use writable directory on Render
+    db_path = '/tmp/render.db'  # ✅ Render allows writing only in /tmp
 
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-
-# from application import create_app
-# from render import render_bp
-# from fetch import fetch_bp
-
-# app = create_app()
-# app.register_blueprint(render_bp)
-# app.register_blueprint(fetch_bp)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
 from wsgi import app  # Import app from the wsgi file
 
 if __name__ == '__main__':
